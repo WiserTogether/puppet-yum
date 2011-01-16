de14f12112bb380c8238650da3e1b34a05164e3e
Atha Kouroussis <akouroussis@gmail.com>
2011-01-15 20:19:54 -0500

    Added yum::localinstall definition to handle installations of packages from local files through yum

---
+++ manifests/definitions/localinstall.pp
@@ -0,0 +1,10 @@
+# Define: yum::localinstall
+# Parameters:
+# $file, $provides
+#
+define yum::localinstall ($file, $provides) {
+	exec { "yum-localinstall-${name}":
+		command => "/usr/bin/yum -d 0 -e 0 -y install ${file}",
+		unless  => "/bin/rpm -q ${provides}"
+	}
+}
