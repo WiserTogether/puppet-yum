b03f7c27f427d972e18a4b0f3b1ad26a942c8b52
Atha Kouroussis <akouroussis@gmail.com>
2011-01-19 12:22:46 -0500

    Added EPEL puppet yum repo class

---
+++ manifests/classes/repo/epel/puppet.pp
@@ -0,0 +1,12 @@
+# Class: yum::repo::epel::puppet
+#
+#
+class yum::repo::epel::puppet {
+	yum::managed_repo { "epel-puppet":
+	    descr          => "Extra Packages for Enterprise Linux $lsbmajdistrelease - $architecture - Puppet",
+		baseurl        => "http://tmz.fedorapeople.org/repo/puppet/epel/${lsbmajdistrelease}/${architecture}/",
+	    enabled        => 1,
+		gpgcheck       => 0,
+	    priority       => 17
+    }
+}
