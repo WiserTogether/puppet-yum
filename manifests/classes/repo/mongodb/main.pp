a831ffc44f8daa00f33483a99840c40dcbc0861d
Atha Kouroussis <akouroussis@gmail.com>
2011-02-05 15:30:24 -0300

    Renamed mongodb repo class

--- manifests/classes/repo/mongodb/main.pp
+++ manifests/classes/repo/mongodb/main.pp
@@ -1,7 +1,7 @@
-# Class: yum::repo::mongodb::10gen
+# Class: yum::repo::mongodb::main
 #
 #
-class yum::repo::mongodb::10gen {
+class yum::repo::mongodb::main {
 	yum::managed_repo { "10gen":
         descr    => "CentOS-${lsbmajdistrelease} - 10gen",
         baseurl  => "http://downloads.mongodb.org/distros/centos/5.4/os/${architecture}/",
