5a88453f815701c4eec7ac4eb06d75a3d769b9b4
Atha Kouroussis <akouroussis@gmail.com>
2011-02-19 19:57:53 -0300

    Refactor directory structure
    - Move files so puppet can autofind/load classes
    - Added opsview repo

---
+++ manifests/repo/opsview.pp
@@ -0,0 +1,11 @@
+# Class: yum::repo::opsview
+#
+#
+class yum::repo::opsview {
+	yum::managed_repo { "opsview":
+	    descr      => "Opsview",
+        baseurl  => "http://downloads.opsera.com/opsview-community/latest/yum/${operatingsystem}/${lsbmajdistrelease}/${architecture}",
+	    enabled    => 1,
+    	gpgcheck   => 0
+	}
+}
