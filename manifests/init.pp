4d825e5ef9429bac667ffdb081f791c5344aaf54
Erik LaBianca <erik.labianca@gmail.com>
2012-08-10 02:34:08 -0400

    remove redhat-lsb dependencies, they do not work
    Conflicts:
    
    	manifests/init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,10 +16,6 @@
 
 class yum {
 	# autoupdate
-	package { 'redhat-lsb':
-		ensure  => present,
-		require => [ Class['yum::repo::base'], Class['yum::repo::extras'] ],
-	}
 	#package { 'yum-cron':
 	#	ensure  => present,
 	#	#require => [ Class['yum::repo::base'], Class['yum::repo::extras'] ],
