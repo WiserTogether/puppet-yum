d846770b6bdd8f1409fe8c77cd2624127291f3fc
Erik LaBianca <erik.labianca@gmail.com>
2012-08-11 21:03:49 -0400

    recognize the fact that pgdg depends on the base repos

--- manifests/repo/pgdg90.pp
+++ manifests/repo/pgdg90.pp
@@ -1,9 +1,10 @@
 class yum::repo::pgdg90 {
+        require yum::repo::base
 	yum::managed_repo { "pgdg90":
 		descr => "PostgreSQL 9.0 \$releasever - \$basearch",
-		      baseurl => "http://yum.pgrpms.org/9.0/redhat/rhel-${lsbmajdistrelease}-${architecture}",
-		      enabled => 1,
-		      gpgcheck => 1,
-		      gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG"
-	}
+		baseurl => "http://yum.pgrpms.org/9.0/redhat/rhel-${lsbmajdistrelease}-${architecture}",
+		enabled => 1,
+		gpgcheck => 1,
+		gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG"
+	} 
 }
