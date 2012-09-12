cc0da83803531b338677630661c762f9814826e1
Erik LaBianca <erik.labianca@wisertogether.com>
2012-09-12 11:24:59 -0400

    prefix epel to repositories dependent on epel

--- manifests/repo/epel/easel.pp
+++ manifests/repo/epel/easel.pp
@@ -2,7 +2,7 @@
 #
 #
 class yum::repo::epel::easel {
-	yum::managed_repo { 'easel':
+	yum::managed_repo { 'epel-easel':
 		descr      => "Easel EPEL",
                 baseurl    => "http://easel.github.com/yum-repo-misc/epel/5/x86_64/",
 		enabled    => 1,
