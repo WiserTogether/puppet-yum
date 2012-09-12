0c455d89050840126db5f2825daee678870ccaa0
Erik LaBianca <erik.labianca@wisertogether.com>
2012-09-12 19:44:40 -0400

    add epel dependency to epel-easel

--- manifests/repo/epel/easel.pp
+++ manifests/repo/epel/easel.pp
@@ -2,10 +2,12 @@
 #
 #
 class yum::repo::epel::easel {
+    require yum::repo::epel::main
 	yum::managed_repo { 'epel-easel':
 		descr      => "Easel EPEL",
-                baseurl    => "http://easel.github.com/yum-repo-misc/epel/5/x86_64/",
+        baseurl    => "http://easel.github.com/yum-repo-misc/epel/5/x86_64/",
 		enabled    => 1,
 		gpgcheck   => 0,
+		require => Yumrepo['epel'],
 	}
 }
