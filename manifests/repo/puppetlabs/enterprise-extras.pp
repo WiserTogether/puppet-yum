b573d3b6f211872c157998fc160d2217a8794c17
Erik LaBianca <erik.labianca@gmail.com>
2012-10-08 19:07:53 -0400

    fix repo url to use lsb major release

--- manifests/repo/puppetlabs/enterprise-extras.pp
+++ manifests/repo/puppetlabs/enterprise-extras.pp
@@ -5,7 +5,7 @@ class yum::repo::puppetlabs::enterprise-extras {
   include deps
 	yum::managed_repo { 'puppetlabs-enterprise-extras':
 		descr    => 'Puppetlabs packages - enterprise extras',
-		baseurl  => 'http://yum-enterprise.puppetlabs.com/el/$releasever/extras/$basearch/',
+		baseurl  => 'http://yum-enterprise.puppetlabs.com/el/${lsbmajdistrelease}/extras/$basearch/',
 		enabled  => 1,
 		gpgcheck => 1,
 		gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
