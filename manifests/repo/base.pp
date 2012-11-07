21617028e6127988761b8051a4ac803b6f8dcbd3
Erik LaBianca <erik.labianca@gmail.com>
2012-11-07 18:14:21 -0500

    try harder to prevent base.repo from being created on redhat

--- manifests/repo/base.pp
+++ manifests/repo/base.pp
@@ -2,11 +2,15 @@
 #
 #
 class yum::repo::base {
-	yum::managed_repo { 'base':
-		descr      => "CentOS-${lsbmajdistrelease} - Base",
-		mirrorlist => "http://mirrorlist.centos.org/?release=${lsbmajdistrelease}&arch=${architecture}&repo=os",
-		enabled    => 1,
-		gpgcheck   => 1,
-		gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5',
-	}
+  case $operatingsystem {
+     Redhat: { notice 'no base repo needed on RHEL' }
+     CentOS: {
+        yum::managed_repo { 'base':
+            descr      => "CentOS-${lsbmajdistrelease} - Base",
+            mirrorlist => "http://mirrorlist.centos.org/?release=${lsbmajdistrelease}&arch=${architecture}&repo=os",
+            enabled    => 1,
+            gpgcheck   => 1,
+            gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5',
+        }
+     }
 }
