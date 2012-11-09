a2a50c3240bd8122b54ee1c77b4016026dc37775
Erik LaBianca <erik.labianca@gmail.com>
2012-11-09 14:55:46 -0500

    use fully qualified paths for puppet labs repo dependencies

--- manifests/repo/puppetlabs/base.pp
+++ manifests/repo/puppetlabs/base.pp
@@ -2,8 +2,8 @@
 #
 #
 class yum::repo::puppetlabs::base {
-  include deps
-  include enterprise-extras
+  include yum::repo::puppetlabs::deps
+  include yum::repo::puppetlabs::enterprise-extras
 	yum::managed_repo { 'puppetlabs-base':
 		descr    => 'Puppetlabs packages - base',
 		baseurl  => 'http://yum.puppetlabs.com/el/$releasever/products/$basearch/',
